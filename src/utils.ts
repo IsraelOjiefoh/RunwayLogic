5707819efe08e6b26b57213653240521186154ae
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-21 17:36:09 -0800

    feat: remove custom occupation display in Occupation component and update API URL for staging environment

--- src/utils.ts
+++ src/utils.ts
@@ -4,8 +4,11 @@ export const GetApiUrl = () => {
   if (isProd) {
     if (window.location.hostname === "runwaylogic.netlify.app") {
       return "https://server-production-c80e.up.railway.app";
+    } else if (window.location.hostname === "runwaylogic-staging.netlify.app") {
+      return "https://server-staging-d0ff.up.railway.app";
     }
   } else {
+    // Development environment
     return "http://localhost:3000";
   }
 };
