6f2e26669c07c03a9ffc115fbd1938efb3cea8dd
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-21 17:58:38 -0800

    feat: add staging environment check for API URL in GetApiUrl utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,13 @@
 export const GetApiUrl = () => {
   const isProd = process.env.NODE_ENV === "production";
+  const isStaging = process.env.NODE_ENV === "staging"; // Check for staging environment
 
   if (isProd) {
     if (window.location.hostname === "runwaylogic.netlify.app") {
       return "https://server-production-c80e.up.railway.app";
-    } else if (window.location.hostname === "runwaylogic-staging.netlify.app") {
+    }
+  } else if (isStaging) {
+    if (window.location.hostname === "runwaylogic-staging.netlify.app") {
       return "https://server-staging-d0ff.up.railway.app";
     }
   } else {
