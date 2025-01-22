a5cb15822069cb85dd28ffc1ded2e69a627f2b40
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-21 18:05:21 -0800

    feat: simplify staging environment check in GetApiUrl utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,13 @@
 export const GetApiUrl = () => {
   const isProd = process.env.NODE_ENV === "production";
-  const isStaging = process.env.NODE_ENV === "staging"; // Check for staging environment
+  const isStaging =
+    window.location.hostname === "runwaylogic-staging.netlify.app"; // Check for staging using hostname
 
   if (isProd) {
-    if (window.location.hostname === "runwaylogic.netlify.app") {
-      return "https://server-production-c80e.up.railway.app";
-    }
+    return "https://server-production-c80e.up.railway.app";
   } else if (isStaging) {
-    if (window.location.hostname === "runwaylogic-staging.netlify.app") {
-      return "https://server-staging-d0ff.up.railway.app";
-    }
+    return "https://server-staging-d0ff.up.railway.app";
   } else {
-    // Development environment
     return "http://localhost:3000";
   }
 };
