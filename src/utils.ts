f9e58e2582dcb20b0f8bc21556b3448ef1ab5c27
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-21 18:15:45 -0800

    feat: reorder environment checks in GetApiUrl utility function for clarity

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,12 @@
 export const GetApiUrl = () => {
-  const isProd = process.env.NODE_ENV === "production";
   const isStaging =
     window.location.hostname === "runwaylogic-staging.netlify.app"; // Check for staging using hostname
+  const isProd = process.env.NODE_ENV === "production";
 
-  if (isProd) {
-    return "https://server-production-c80e.up.railway.app";
-  } else if (isStaging) {
+  if (isStaging) {
     return "https://server-staging-d0ff.up.railway.app";
+  } else if (isProd) {
+    return "https://server-production-c80e.up.railway.app";
   } else {
     return "http://localhost:3000";
   }
