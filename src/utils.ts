1614a5a48b73546baef450d2fb7db9655a45d7d5
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-22 17:04:00 -0800

    feat: remove unnecessary logging for environment detection in GetApiUrl utility function

--- src/utils.ts
+++ src/utils.ts
@@ -2,13 +2,10 @@ export const GetApiUrl = () => {
   console.log("Current hostname: ", window.location.hostname); // Log current hostname
 
   if (window.location.hostname === "runwaylogic-staging.netlify.app") {
-    console.log("Staging detected");
     return "https://server-staging-d0ff.up.railway.app";
   } else if (window.location.hostname === "runwaylogic.netlify.app") {
-    console.log("Production detected");
     return "https://server-production-c80e.up.railway.app";
   } else {
-    console.log("Default (Localhost) detected");
     return "http://localhost:3000";
   }
 };
