e248e4cef3d96c6ff9f74776a8ce43c0db1542e0
Israel Ojiefoh <israelmayowa580@gmail.com>
2025-01-24 10:25:48 -0800

    feat: add ProfileSettings component and associated assets

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from "./components/account creation/CreateAccount";
 import OtpVerification from "./components/account creation/otp";
 import Homepage from "./components/homepage/Homepage";
 import Login from "./components/login page/Login";
+/* import ProfileSettings from "./components/profile settings/ProfileSettings"; */
 
 function App() {
   return (
